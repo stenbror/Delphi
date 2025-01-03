cd216446a79fe64ebbbd54ca5a99be4aa12e9aa4
Richard Magnor Stenbro <stenbror@hotmail.com>
2025-01-03 22:25:14 +0100

    Started on scanner class

--- delphi_compiler.pas
+++ delphi_compiler.pas
@@ -4,8 +4,11 @@ program DelphiCompiler(input, output);
 uses
     scanner, parser;
 
+var
+    lexer: TScanner;
 
 begin
     writeLn('Open Delphi compiler, version 0.00');
 
+    lexer := TScanner.init;
 end.
--- scanner.pas
+++ scanner.pas
@@ -2,6 +2,22 @@ unit scanner;
 
 interface
 
+type
+
+    (* Token code used by Parser *)
+    {$scopedEnums on}
+    Token = ( EOF );
+
+    (* Tokenizer for Delphi grammar language *)
+    TScanner = class
+        constructor init;
+    end;
+
 implementation
 
+    constructor TScanner.init;
+    begin
+        
+        writeLn('TScanner:  Initializing!');
+    end;
 end.
